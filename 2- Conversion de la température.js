f0f971f72055ecfd7bc30f0395c00ba1c5cbcf24
zineb.hssini03891 <zainab.malak092@gmail.com>
2026-09-18 12:34:11 +0200

    rename challenge

--- 2- Conversion de la température.js
+++
@@ -1,4 +0,0 @@
-const prompt = require("prompt-sync")()
-let temperatureCelsius = prompt("Entrez la température en degrés Celsius : ");
-let temperatureFahrenheit = temperatureCelsius + 273.15;
-console.log(`La température en degrés Fahrenheit est : ${temperatureFahrenheit}`)
