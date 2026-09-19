45bf79031da855f16f20807c5506b2a005c89cc8
zineb.hssini03891 <zainab.malak092@gmail.com>
2026-09-19 11:08:35 +0200

    Add challenge

---
+++ 4-Conversion de la vitesse.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+let vitesse = Number(prompt("Entrer la vitesse en Km/h: "));
+let vitesseConvertie = vitesse * 0.27778;
+
+console.log(`La vitesse en m/s est: ${vitesseConvertie}`)
